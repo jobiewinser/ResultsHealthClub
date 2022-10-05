0aec35e6874a89f71274c534c6485316929883be
Jobie Winser <jobiewinser@live.co.uk>
2022-10-05 22:50:40 +0100

    fix?

--- WinserSystems/routing.py
+++ WinserSystems/routing.py
@@ -2,13 +2,15 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
 from messaging import consumers
-
+import os
 # URLs that handle the WebSocket connection are placed here.
 websocket_urlpatterns=[
                     re_path(
                         r"ws/chat/(?P<chat_box_whatsapp_number>\w+)/(?P<chat_box_site_pk>\w+)/$", consumers.ChatRoomConsumer.as_asgi()
                     ),
                 ]
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WinserSystems.settings')
 
 application = ProtocolTypeRouter( 
     {
