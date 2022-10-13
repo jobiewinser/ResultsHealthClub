5b02a25d49bd0403eac1c4ab09aacbfa15472276
jobie winser <jobie.winser@abingdon.org.uk>
2022-10-13 09:07:00 +0100

    calendly-webhooks/ fix

--- calendly/urls.py
+++ calendly/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.urls import path
 import calendly.views as calendlyviews
 urlpatterns = [
-    path('calendly-webhooks/<str:guid>/', calendlyviews.Webhooks.as_view(), name='calendly-webhooks' ),
+    path('calendly-webhooks/', calendlyviews.Webhooks.as_view(), name='calendly-webhooks' ),
     
 ]
