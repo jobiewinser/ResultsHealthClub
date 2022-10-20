3d49686e6e310f6d07ada0ba2ea0aee995f743b4
jobie winser <jobie.winser@abingdon.org.uk>
2022-10-20 11:27:09 +0100

    lead admin added

--- campaign_leads/admin.py
+++ campaign_leads/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from campaign_leads.models import Campaignlead
+
 # Register your models here.
+
+class CampaignleadAdmin(admin.ModelAdmin):
+    list_display = ['first_name','last_name','email','whatsapp_number',
+    'campaign','created','arrived','sold','complete',
+    'active_campaign_contact_id','active_campaign_form_id',
+    'possible_duplicate','last_dragged']
+    search_fields = ['first_name','last_name','email','whatsapp_number',
+    'campaign','created','arrived','sold','complete',
+    'active_campaign_contact_id','active_campaign_form_id',
+    'possible_duplicate','last_dragged']
+admin.site.register(Campaignlead, CampaignleadAdmin)
+
+
+    
