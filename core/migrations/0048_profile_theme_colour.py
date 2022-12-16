2a70ad8e9df5a9541ca9c500bd9f9fc65773e631
jobie winser <jobiewinser@live.co.uk>
2022-12-16 01:57:02 +0000

    tweak

--- core/migrations/0048_profile_theme_colour.py
+++ core/migrations/0048_profile_theme_colour.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='profile',
             name='theme_colour',
-            field=models.CharField(default='0, 0, 255', max_length=20),
+            field=models.CharField(blank=True, default='', max_length=20, null=True),
         ),
     ]
